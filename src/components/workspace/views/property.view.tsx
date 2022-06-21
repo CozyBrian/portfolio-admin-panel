7c2f048c3b6dde30b3586f66d7b5ff321a91bf38
CozyBrian <dragonbrian33@gmail.com>
2022-06-21 23:43:45 +0000

    lazy revert

--- src/components/workspace/views/property.view.tsx
+++ src/components/workspace/views/property.view.tsx
@@ -1,10 +1,5 @@
 import { useStateContext } from "../../../context/stateContext";
-<<<<<<< HEAD
 import { TbUpload } from "react-icons/tb";
-import { IoTrash } from "react-icons/io5";
-import { BsThreeDotsVertical } from "react-icons/bs";
-=======
->>>>>>> parent of 82ed5cd (feat: updating projects)
 import { Inputs } from "../../inputs";
 import "./propertyview.css";
 
@@ -12,44 +7,23 @@ const PropertiesView = () => {
   const state = useStateContext();
 
   if (!state) return null;
-  const { pActive, curObject } = state;
+  const { pActive, curObject, publish } = state;
 
   return (
     <div className="PropertiesView">
-<<<<<<< HEAD
       <div className="view-title-bar">
         <h3 className="view-title">{pActive}</h3>
-        <div className="title-icons">
-          <IoTrash className="delete-icon" color="red" size={22} />
-          <div className="tbutton" onClick={() => publish()}>
-            <TbUpload size={22} />
-            <div>{" Publish"}</div>
-          </div>
-          <BsThreeDotsVertical size={22} />
+        <div className="tbutton" onClick={() => publish()}>
+          <TbUpload size={22} />
+          <div>{" Publish"}</div>
         </div>
       </div>
       <hr className="hr" />
-=======
-      <h3 className="view-title">{pActive}</h3>
-      <hr />
->>>>>>> parent of 82ed5cd (feat: updating projects)
       {curObject && (
         <div className="fields-view">
           <Inputs obj={curObject} />
         </div>
       )}
-      <hr className="hr b" />
-      <div className="view-title-bar">
-        <h3 className="view-title">{pActive}</h3>
-        <div className="title-icons">
-          <IoTrash className="delete-icon" color="red" size={22} />
-          <div className="tbutton" onClick={() => publish()}>
-            <TbUpload size={22} />
-            <div>{" Publish"}</div>
-          </div>
-          <BsThreeDotsVertical size={22} />
-        </div>
-      </div>
     </div>
   );
 };
