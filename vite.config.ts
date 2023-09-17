7658bf666823eb760ade7ef0039f561b20007de7
CozyBrian <dragonbrian33@gmail.com>
2023-09-17 13:22:42 +0000

    bun?

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import * as path from "path";
 import react from "@vitejs/plugin-react";
+import ClosePlugin from "./vite-plugin-close";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ClosePlugin()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
